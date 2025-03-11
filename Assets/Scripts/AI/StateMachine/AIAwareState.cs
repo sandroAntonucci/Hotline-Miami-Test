f9afc00e80b17fd81ec179a0d743439def68ab2b
Tottii05 <146985184+Tottii05@users.noreply.github.com>
2025-03-11 15:55:14 +0100

    Added every single thing you can imagine for an AI - Sun Tzu, The Art Of War

--- Assets/Scripts/AI/StateMachine/AIAwareState.cs
+++ Assets/Scripts/AI/StateMachine/AIAwareState.cs
@@ -6,22 +6,12 @@ public class AIAwareState : AIBaseState
 {
     public override void EnterState(AIHandler handler)
     {
-
+        handler.ChangeState(handler.chasingState);
     }
 
     public override void UpdateState(AIHandler handler)
     {
-        bool playerInVision = handler.IsPlayerInVision();
-        if (playerInVision)
-        {
-            if (handler.IsInAttackRange())
-                handler.ChangeState(handler.attackingState);
-            else
-                handler.ChangeState(handler.chasingState);
-        } else
-        {
-            handler.ChangeState(handler.chasingState);
-        }
+
     }
 
     public override void ExitState(AIHandler handler)
